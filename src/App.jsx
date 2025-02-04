5e3ba9e45682f502c1aad7ed4151f7334b9b06b8
Viktor <viktorharisov@mail.ru>
2025-02-04 21:20:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <text>АЛООО</text>
+      </div>
+    </>
+  )
+}
+
+export default App
